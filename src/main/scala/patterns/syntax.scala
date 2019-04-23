18e331ab8d2938e5a1d9208e448071e3f5ed7e7f
Kyle Dewey <kyle.dewey@csun.edu>
2019-04-22 17:18:48 -0700

    typechecker compiling; still untested

--- src/main/scala/patterns/syntax.scala
+++ src/main/scala/patterns/syntax.scala
@@ -1,15 +1,15 @@
 package patterns
 
-case class TypeName(name: String)
+case class UserDefinedTypeName(name: String)
 case class Variable(name: String)
 case class ConstructorName(name: String)
 
 sealed trait Type
 case object IntType extends Type
-case class UserDefinedType(name: TypeName) extends Type
+case class UserDefinedType(name: UserDefinedTypeName) extends Type
 
 case class ConstructorDefinition(name: ConstructorName, params: Seq[Type])
-case class TypeDefinition(name: TypeName, constructors: Seq[ConstructorDefinition])
+case class TypeDefinition(name: UserDefinedTypeName, constructors: Seq[ConstructorDefinition])
 
 sealed trait Pattern
 case class VariablePattern(variable: Variable) extends Pattern
@@ -19,5 +19,6 @@ case class Case(pattern: Pattern, code: Expression)
 
 sealed trait Expression
 case class IntExpression(value: Int) extends Expression
+case class LetExpression(variable: Variable, value: Expression, in: Expression) extends Expression
 case class CallConstructor(name: ConstructorName, params: Seq[Expression]) extends Expression
 case class PatternMatch(on: Expression, cases: Seq[Case]) extends Expression
---
+++ src/main/scala/patterns/typechecker.scala
@@ -0,0 +1,162 @@
+package patterns
+
+case class TypeErrorException(msg: String) extends Exception(msg)
+
+case class ConstructorInfo(
+  correspondingUserType: UserDefinedTypeName,
+  expectedTypes: Seq[Type])
+
+class Typechecker(
+  val constructorsForType: Map[UserDefinedTypeName, Set[ConstructorName]],
+  val constructorInfo: Map[ConstructorName, ConstructorInfo]) {
+
+  def getConstructorInfo(name: ConstructorName, numParams: Int): ConstructorInfo = {
+    val result = constructorInfo.getOrElse(
+      name,
+      throw TypeErrorException("Unknown constructor name: " + name))
+    if (result.expectedTypes.size != numParams) {
+      throw TypeErrorException("Constructor arity mismatch: " + name)
+    }
+    result
+  } // getConstructorInfo
+
+  def joinSeqs(seq1: Seq[MatchValue], seq2: Seq[MatchValue]): Seq[MatchValue] = {
+    assert(seq1.size == seq2.size)
+    seq1.zip(seq2).map(
+      { case (first, second) => first.join(second) })
+  } // joinSeqs
+
+  def allHandled(forType: UserDefinedTypeName, map: Map[ConstructorName, Seq[MatchValue]]): Boolean = {
+    val allConstructors = constructorsForType(forType)
+    map.keySet == allConstructors && map.values.forall(_.forall(_.isMatchAll))
+  } // allHandled
+
+  def joinMaps(
+    map1: Map[ConstructorName, Seq[MatchValue]],
+    map2: Map[ConstructorName, Seq[MatchValue]]): Map[ConstructorName, Seq[MatchValue]] = {
+    val onlyInMap1 = map1.filterKeys(k => !map2.contains(k))
+    val onlyInMap2 = map2.filterKeys(k => !map1.contains(k))
+    val inBoth = map1.keySet.intersect(map2.keySet)
+
+    inBoth.map(k => (k, joinSeqs(map1(k), map2(k)))).toMap ++ onlyInMap1 ++ onlyInMap2
+  } // joinMaps
+
+  sealed trait MatchValue {
+    def join(other: MatchValue): MatchValue
+    def isMatchAll: Boolean
+  }
+  case object MatchAll extends MatchValue {
+    def join(other: MatchValue): MatchValue = {
+      throw TypeErrorException("Pattern will never be reached")
+    }
+    def isMatchAll: Boolean = true
+  }
+  case object MatchNone extends MatchValue {
+    def join(other: MatchValue): MatchValue = other
+    def isMatchAll: Boolean = false
+  }
+  case class MatchSome(
+    forType: UserDefinedTypeName,
+    matchedSoFar: Map[ConstructorName, Seq[MatchValue]]) extends MatchValue {
+
+    def join(other: MatchValue): MatchValue = {
+      other match {
+        case MatchAll => MatchAll
+        case MatchNone => this
+        case MatchSome(otherForType, otherMatchedSoFar) => {
+          if (forType != otherForType) {
+            throw TypeErrorException("Type mismatch in pattern")
+          }
+          val resultMap = joinMaps(matchedSoFar, otherMatchedSoFar)
+          if (allHandled(forType, resultMap)) {
+            MatchAll
+          } else {
+            MatchSome(forType, resultMap)
+          }
+        }
+      }
+    } // join
+
+    def isMatchAll: Boolean = false
+  } // MatchSome
+
+  def patternToMatchValue(pattern: Pattern): MatchValue = {
+    pattern match {
+      case VariablePattern(_) => MatchAll
+      case ConstructorPattern(name, params) => {
+        val ConstructorInfo(userType, expectedTypes) = getConstructorInfo(name, params.size)
+        MatchSome(
+          userType,
+          Map(name -> params.map(patternToMatchValue)))
+      }
+    }
+  } // patternToMatchValue
+
+  def patternVariables(contextType: Type, pattern: Pattern): Map[Variable, Type] = {
+    pattern match {
+      case VariablePattern(variable) => Map(variable -> contextType)
+      case ConstructorPattern(name, params) => {
+        contextType match {
+          case UserDefinedType(contextTypeName) => {
+            val ConstructorInfo(expectedUserType, expectedTypes) = getConstructorInfo(name, params.size)
+            if (contextTypeName != expectedUserType) {
+              throw TypeErrorException("Expected: " + expectedUserType + "; received: " + contextTypeName)
+            }
+            assert(params.size == expectedTypes.size)
+            expectedTypes.zip(params).foldLeft(Map[Variable, Type]())((res, cur) => {
+              val curResult = patternVariables(cur._1, cur._2)
+              if (curResult.keySet.intersect(res.keySet).nonEmpty) {
+                throw TypeErrorException("Duplicate variable introduced in pattern")
+              }
+              res ++ curResult
+            })
+          }
+          case _ => throw TypeErrorException("Expected user type; got: " + contextType)
+        }
+      }
+    }
+  } // patternVariables
+
+  def typeof(exp: Expression, env: Map[Variable, Type]): Type = {
+    exp match {
+      case IntExpression(_) => IntType
+      case LetExpression(variable, value, in) => {
+        val valueType = typeof(value, env)
+        typeof(in, env + (variable -> valueType))
+      }
+      case CallConstructor(name, params) => {
+        val ConstructorInfo(typeName, expectedTypes) = getConstructorInfo(name, params.size)
+        if (params.map(typeof(_, env)) != expectedTypes) {
+          throw TypeErrorException("Constructor parameter type mismatch: " + name)
+        }
+        UserDefinedType(typeName)
+      }
+      case PatternMatch(on, cases) => {
+        typeof(exp, env) match {
+          case userType@UserDefinedType(_) => {
+            val (Some(finalReturn), finalMatch) =
+              cases.foldLeft((None: Option[Type], MatchNone: MatchValue))((res, cur) => {
+                val (returnType, curMatch) = res
+                val Case(curPattern, curExp) = cur
+                val nextMatch = curMatch.join(patternToMatchValue(curPattern))
+                val addVariables = patternVariables(userType, curPattern)
+                val curReturnType = typeof(curExp, env ++ addVariables)
+                val nextReturnType = returnType match {
+                  case None | Some(`curReturnType`) => Some(curReturnType)
+                  case _ => throw TypeErrorException("Not all cases return same type")
+                }
+                (nextReturnType, nextMatch)
+              })
+            if (!finalMatch.isMatchAll) {
+              throw TypeErrorException("Non-exhaustive pattern match")
+            }
+            finalReturn
+          }
+          case other => {
+            throw TypeErrorException("Can only pattern match on user types; found: " + other)
+          }
+        }
+      }
+    }
+  } // typeof
+} // Typechecker
