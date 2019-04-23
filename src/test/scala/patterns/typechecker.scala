1c923afab585935335b840af7e69fc0565e09e10
Kyle Dewey <kyle.dewey@csun.edu>
2019-04-22 18:16:03 -0700

    added a bunch more tests; failure observed involving nested patterns incorrectly stating that a pattern will never be reached

--- src/test/scala/patterns/typechecker.scala
+++ src/test/scala/patterns/typechecker.scala
@@ -116,4 +116,180 @@ class TestTypechecker extends FlatSpec {
             Case(VariablePattern(Variable("x")), VariableExpression(Variable("x")))))))
     }
   }
+
+  it should "accept a well-typed pattern match - all cases stated at toplevel" in {
+    assertResult(IntType) {
+      typecheck(Program(Seq(INT_LIST_DEFINITION),
+        PatternMatch(
+          CallConstructor(NIL_NAME, Seq()),
+          Seq(
+            Case(
+              // Cons(head, tail) => head
+              ConstructorPattern(CONS_NAME, Seq(VariablePattern(Variable("head")), VariablePattern(Variable("tail")))),
+              VariableExpression(Variable("head"))),
+            Case(
+              // Nil => 0
+              ConstructorPattern(NIL_NAME, Seq()),
+              IntExpression(0))))))
+    }
+  }
+
+  it should "accept a well-typed pattern match - specific followed by catch-all at toplevel" in {
+    assertResult(IntType) {
+      typecheck(Program(Seq(INT_LIST_DEFINITION),
+        PatternMatch(
+          CallConstructor(NIL_NAME, Seq()),
+          Seq(
+            Case(
+              // Cons(head, tail) => head
+              ConstructorPattern(CONS_NAME, Seq(VariablePattern(Variable("head")), VariablePattern(Variable("tail")))),
+              VariableExpression(Variable("head"))),
+            Case(
+              // x => 0
+              VariablePattern(Variable("x")),
+              IntExpression(0))))))
+    }
+  }
+
+  it should "reject an ill-typed pattern match: different types on different arms" in {
+    assertThrows[TypeErrorException] {
+      typecheck(Program(Seq(INT_LIST_DEFINITION),
+        PatternMatch(
+          CallConstructor(NIL_NAME, Seq()),
+          Seq(
+            Case(
+              // Cons(head, tail) => tail
+              ConstructorPattern(CONS_NAME, Seq(VariablePattern(Variable("head")), VariablePattern(Variable("tail")))),
+              VariableExpression(Variable("tail"))),
+            Case(
+              // Nil => 0
+              ConstructorPattern(NIL_NAME, Seq()),
+              IntExpression(0))))))
+    }
+  }
+
+  it should "reject an ill-typed pattern match: dead code due to catch-all" in {
+    assertThrows[TypeErrorException] {
+      typecheck(Program(Seq(INT_LIST_DEFINITION),
+        PatternMatch(
+          CallConstructor(NIL_NAME, Seq()),
+          Seq(
+            Case(
+              // x => x
+              VariablePattern(Variable("x")),
+              VariableExpression(Variable("x"))),
+            Case(
+              // Nil => 0
+              ConstructorPattern(NIL_NAME, Seq()),
+              IntExpression(0))))))
+    }
+  }
+
+  it should "reject an ill-typed pattern match: dead code due to duplicate pattern" in {
+    assertThrows[TypeErrorException] {
+      typecheck(Program(Seq(INT_LIST_DEFINITION),
+        PatternMatch(
+          CallConstructor(NIL_NAME, Seq()),
+          Seq(
+            Case(
+              // Cons(head, tail) => head
+              ConstructorPattern(CONS_NAME, Seq(VariablePattern(Variable("head")), VariablePattern(Variable("tail")))),
+              VariableExpression(Variable("head"))),
+            Case(
+              // Cons(head, tail) => head
+              ConstructorPattern(CONS_NAME, Seq(VariablePattern(Variable("head")), VariablePattern(Variable("tail")))),
+              VariableExpression(Variable("head"))),
+            Case(
+              // Nil => 0
+              ConstructorPattern(NIL_NAME, Seq()),
+              IntExpression(0))))))
+    }
+  }
+
+  it should "reject an ill-typed pattern match: non-exhaustive pattern match at toplevel" in {
+    assertThrows[TypeErrorException] {
+      typecheck(Program(Seq(INT_LIST_DEFINITION),
+        PatternMatch(
+          CallConstructor(NIL_NAME, Seq()),
+          Seq(
+            Case(
+              // Cons(head, tail) => head
+              ConstructorPattern(CONS_NAME, Seq(VariablePattern(Variable("head")), VariablePattern(Variable("tail")))),
+              VariableExpression(Variable("head")))))))
+    }
+  }
+
+  it should "reject an ill-typed pattern match: type error in pattern - wrong type" in {
+    assertThrows[TypeErrorException] {
+      typecheck(Program(Seq(INT_LIST_DEFINITION),
+        PatternMatch(
+          CallConstructor(NIL_NAME, Seq()),
+          Seq(
+            Case(
+              // Cons(Cons(head, tail1), tail2) => tail2
+              ConstructorPattern(
+                CONS_NAME,
+                Seq(
+                  ConstructorPattern(
+                    CONS_NAME,
+                    Seq(
+                      VariablePattern(Variable("head")),
+                      VariablePattern(Variable("tail1")))),
+                  VariablePattern(Variable("tail2")))),
+              VariableExpression(Variable("tail2")))))))
+    }
+  }
+
+  it should "reject an ill-typed pattern match: type error in pattern - arity mismatch" in {
+    assertThrows[TypeErrorException] {
+      typecheck(Program(Seq(INT_LIST_DEFINITION),
+        PatternMatch(
+          CallConstructor(NIL_NAME, Seq()),
+          Seq(
+            Case(
+              // Cons(head, tail1, tail2) => head
+              ConstructorPattern(
+                CONS_NAME,
+                Seq(
+                  VariablePattern(Variable("head")),
+                  VariablePattern(Variable("tail1")),
+                  VariablePattern(Variable("tail2")))),
+              VariableExpression(Variable("head"))),
+            Case(
+              // Nil => 0
+              ConstructorPattern(NIL_NAME, Seq()),
+              IntExpression(0))))))
+    }
+  }
+
+  it should "accept a well-typed pattern match: nested patterns - catch-all" in {
+    assertResult(IntType) {
+      typecheck(Program(Seq(INT_LIST_DEFINITION),
+        PatternMatch(
+          CallConstructor(NIL_NAME, Seq()),
+          Seq(
+            Case(
+              // Cons(head1, Cons(head2, tail)) => 0
+              ConstructorPattern(
+                CONS_NAME,
+                Seq(
+                  VariablePattern(Variable("head1")),
+                  ConstructorPattern(
+                    CONS_NAME,
+                    Seq(
+                      VariablePattern(Variable("head2")),
+                      VariablePattern(Variable("tail")))))),
+              IntExpression(0)),
+            Case(
+              // Cons(head, tail) => 1
+              ConstructorPattern(
+                CONS_NAME,
+                Seq(VariablePattern(Variable("head")), VariablePattern(Variable("tail")))),
+              IntExpression(1)),
+            Case(
+              // x => 2
+              VariablePattern(Variable("x")),
+              IntExpression(2))))))
+    }
+  }
 } // TestTypechecker
